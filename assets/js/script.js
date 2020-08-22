1cbbe75a15b76df39d15ee2f840d02b3f151a0f4
catse2000 <catse2000@yahoo.com>
2020-08-21 22:41:27 -0500

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,23 @@ var tasksToDoEl = document.querySelector("#tasks-to-do"); //save query for "#tas
 
 var createTaskHandler = function() {
     event.preventDefault(); //prevents the browser from refreshing after button click
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li"); //stores the ability to create "li" in a variable
     listItemEl.className = "task-item"; // stores a new class name for each element stored in listItemEl
-    listItemEl.textContent = "This is a new task."; //assigns a new text content to the element stored in listItemEl
-    tasksToDoEl.appendChild(listItemEl); //searchs for the element stored in tasksToDoEl and children elements of what is stored in listItemEl
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl); //append child of "div.task-info" to "li"
+    
+    tasksToDoEl.appendChild(listItemEl); //append child of "li" to "ul"
 }
 
 formEl.addEventListener("submit", createTaskHandler) //this allows the event listener to respond to the "submit" button being clicked, or the "Enter Key" being clicked
