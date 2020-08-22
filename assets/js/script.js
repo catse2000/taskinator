45e63c84f938b78c5693df778d2bc9fcb2e3dfc8
catse2000 <catse2000@yahoo.com>
2020-08-21 21:47:45 -0500

    add variable to store query of form and changed click even to submit event for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task"); //save query for "#save-task" to a variable
+var formEl = document.querySelector("#task-form"); //stores query for form in formEl
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //save query for "#tasks-to-do" to a variable
 
 var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+    event.preventDefault(); //prevents the browser from refreshing after button click
+    var listItemEl = document.createElement("li"); //stores the ability to create "li" in a variable
+    listItemEl.className = "task-item"; // stores a new class name for each element stored in listItemEl
+    listItemEl.textContent = "This is a new task."; //assigns a new text content to the element stored in listItemEl
+    tasksToDoEl.appendChild(listItemEl); //searchs for the element stored in tasksToDoEl and children elements of what is stored in listItemEl
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler) //this allows the event listener to respond to the "submit" button being clicked, or the "Enter Key" being clicked
