f59e42d78ea8339364cc2bb8797309599238546c
catse2000 <catse2000@yahoo.com>
2020-08-23 14:33:47 -0500

    added feature to allow tasks to be moved between status areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form"); //stores query for form in fo
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //save query for "#tasks-to-do" to a variable
 var taskIdCounter = 0; //variable to store unique IDs for each task
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function() {
     event.preventDefault(); //prevents the browser from refreshing after button click
@@ -154,11 +156,36 @@ var completeEditTask = function(taskName, taskType, taskId){
 
     formEl.removeAttribute("data-task-id"); //remove data-task-id attribute so there's no confusion when creating new tasks
     document.querySelector("#save-task").textContent = "Add Task"; //change textContent of #save-task button to "Add Task"
-}
+};
 
 //function handles the deletion of a task
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']"); //locate an element with the class of ".task-item" and a "data-task-id" that matches the "taskId" and save it to a variable
     taskSelected.remove();
-}
+};
+
+//function handles the change of the type of task
+var taskStatusChangeHandler = function (event){
+    //get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    //get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+
+    //find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if(statusValue === "to do"){
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if(statusValue == "in progress"){
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if(statusValue == "completed"){
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
+
 pageContentEl.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
